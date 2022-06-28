cd79e0e6f94e3e25afa88e1bf71f932525db5ff2
Anneli Savchenko <95057484+Anneli-sf@users.noreply.github.com>
2022-06-28 13:22:17 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function toReadable(number) {
+module.exports = function toReadable (number) {
     
 const numbersArray = [
         [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
@@ -10,19 +10,19 @@ const numbersArray = [
   let firstNumber = +number.toString().slice(0,1);
   
   if (number < 20 )
-    return console.log(readableN = numbersArray[0][number]);
+    return readableN = numbersArray[0][number];
     
   else if (number >= 20 && number < 100 && number % 10 == 0) 
-    return console.log(readableN = numbersArray[1][firstNumber]);
+    return readableN = numbersArray[1][firstNumber];
   else if (number >= 20 && number < 100 && number % 10 !== 0)   
-    return console.log(readableN = numbersArray[1][firstNumber] + ' ' + numbersArray[0][lastNumber]);
+    return readableN = numbersArray[1][firstNumber] + ' ' + numbersArray[0][lastNumber];
     
   else if (number >= 100 && number % 100 == 0)
-    return console.log(readableN = `${numbersArray[0][firstNumber]} hundred`);
+    return readableN = `${numbersArray[0][firstNumber]} hundred`;
   else if (number >= 100 && number % 100 < 20)   
-    return console.log(readableN = `${numbersArray[0][firstNumber]} hundred ${numbersArray[0][number % 100]}`);
+    return readableN = `${numbersArray[0][firstNumber]} hundred ${numbersArray[0][number % 100]}`;
   else if (number >= 100 && number % 100 >= 20)   
-    return console.log(readableN = `${numbersArray[0][firstNumber]} hundred ${numbersArray[1][firstNumber]} ${numbersArray[0][lastNumber]}`);
+    return readableN = `${numbersArray[0][firstNumber]} hundred ${numbersArray[1][firstNumber]} ${numbersArray[0][lastNumber]}`;
     
     }
 
